375250624c8a5cb92c78603556ddda82047c7b61
sunmin <dltjsals6356@naver.com>
2022-10-26 21:11:56 +0900

    api

--- @HighJava/src/JavaXMLTest/src/kr/or/ddit/basic/assign.java
+++ @HighJava/src/JavaXMLTest/src/kr/or/ddit/basic/assign.java
@@ -17,15 +17,15 @@ public class assign {
 
 	public void parse() throws ParserConfigurationException, SAXException, IOException {
 		
-		String svcKey = "ServiceResult"; // 레시피 재료 정보 조회 서비스
+		//String svcKey = "ServiceResult"; // 레시피 재료 정보 조회 서비스
 		String apiKey = "6QxACXzZz3DhIcHKtEVaNe3%2BNmsF5U7Ql635CDNUki%2Bn7s%2BOIUuMp1MY9rCEsUPerlOfrGwmtgYvnOPWHL2iDw%3D%3D"; // 개인별 발급.
 		String startIdx = "1"; // 레시피 재료 시작 순번
-		String endIdx = "1"; // 레시피 재료 종료 순번
-		String recipeId = "1"; // 래시피가 궁금한 음식 ID
+		String endIdx = "3"; // 레시피 재료 종료 순번
+		//String recipeId = "1"; // 래시피가 궁금한 음식 ID
 
-		URL url = new URL("http://apis.data.go.kr/6300000/festivalDaejeonService/festivalDaejeonList?serviceKey=" + apiKey + "/xml/" + svcKey + "/" + startIdx + "/"
-				+ endIdx + "?RECIPE_ID=" + recipeId);
+		URL url = new URL("http://apis.data.go.kr/6300000/festivalDaejeonService/festivalDaejeonList?serviceKey=" + apiKey + "&startIdx= " +   startIdx + "&endIdx=" + endIdx);
 	
+		
 		//XML문서를 생성하기 위한 DocumentBuilder 객체 생성하기
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = dbf.newDocumentBuilder();
@@ -35,11 +35,10 @@ public class assign {
 		System.out.println("루트엘리먼트 태그명: " + root.getTagName());
 		
 		//전체 컬럼 수를 가져오기
-		String totalCnt = root.getElementsByTagName("totalCount").item(0).getTextContent();
-		endIdx = totalCnt;
+//		String totalCnt = root.getElementsByTagName("totalCount").item(0).getTextContent();
+//		endIdx = totalCnt;
 
-		url = new URL("http://apis.data.go.kr/6300000/festivalDaejeonService/festivalDaejeonList?serviceKey=" + apiKey + "/xml/" + svcKey + "/" + startIdx + "/"
-				+ endIdx + "?RECIPE_ID=" + recipeId);
+		//url = new URL("http://apis.data.go.kr/6300000/festivalDaejeonService/festivalDaejeonList?serviceKey=" + apiKey + "&startIdx= " +   startIdx + "&endIdx=" + endIdx);
 		
 		document = builder.parse(url.toString());
 		
@@ -47,7 +46,7 @@ public class assign {
 		String code = root.getElementsByTagName("returnCode").item(0).getTextContent();
 		
 		if(code.equals("00")) { //정상상태인경우..
-			NodeList rowNodeList = root.getElementsByTagName("row");
+			NodeList rowNodeList = root.getElementsByTagName("items");
 			
 			for (int i = 0; i < rowNodeList.getLength(); i++) {
 				
@@ -55,23 +54,36 @@ public class assign {
 				
 				String auspiceAgency = element.getElementsByTagName("auspiceAgency").item(0).getTextContent();
 				String content = element.getElementsByTagName("content").item(0).getTextContent();
-				String dataStnDt = element.getElementsByTagName("dataStnDt").item(0).getTextContent();
 				String homepageUrl = element.getElementsByTagName("homepageUrl").item(0).getTextContent();
 				String manageAgency = element.getElementsByTagName("manageAgency").item(0).getTextContent();
 				String ntatcSeq = element.getElementsByTagName("ntatcSeq").item(0).getTextContent();
-				String opnEndDt = element.getElementsByTagName("opnEndDt").item(0).getTextContent();
+				String openPlace = element.getElementsByTagName("openPlace").item(0).getTextContent();
 				String opnStrtDt = element.getElementsByTagName("opnStrtDt").item(0).getTextContent();
-				String regDtTm = element.getElementsByTagName("regDtTm").item(0).getTextContent();
-				String tel = element.getElementsByTagName("tel").item(0).getTextContent();
+				String opnEndDt = element.getElementsByTagName("opnEndDt").item(0).getTextContent();
 				String title = element.getElementsByTagName("title").item(0).getTextContent();
+				String tel = element.getElementsByTagName("tel").item(0).getTextContent();
 				
-				System.out.printf("%3s %8s %10s %10s %8s %8s %8s %8s %8s %8s\n", auspiceAgency, content, dataStnDt, homepageUrl, manageAgency, ntatcSeq, opnEndDt, opnStrtDt, regDtTm, tel, title);
+				System.out.println("축제번호 : " + ntatcSeq);
+				System.out.println("축제명 : " + title);
+				System.out.println("내용 : " + content);
+				System.out.println("시작일 : " + opnStrtDt);
+				System.out.println("종료일 : " + opnEndDt);
+				System.out.println("개최장소 : " + openPlace);
+				System.out.println("주최기관 : " + auspiceAgency);
+				System.out.println("주관기관 : " + manageAgency);
+				System.out.println("연락처 : " + tel);
+				System.out.println("홈페이지 : " + homepageUrl);
 				System.out.println("---------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+			
+				
+				
+				System.out.printf("%8s\r %15s\r %50s\r %8s\r %8s\r %15s\r %8s\r %8s\r %8s\r %10s\n" , ntatcSeq, title, content, opnStrtDt, opnEndDt, openPlace, auspiceAgency, manageAgency, tel, homepageUrl);
+			
 			}
 		}	
 	}
 	
-	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+	public static void main(String[] args) throws Exception  {
 		new assign().parse();
 	}
 
